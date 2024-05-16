7d536d31357054ab59fab6c191c47cba457bbb1c
BrianDinh22 <briandinh2022@u.northwestern.edu>
2024-05-16 13:36:00 -0500

    attempt at data cleaning for social network analysis prep

--- 01_data_cleaning.R
+++ 01_data_cleaning.R
@@ -22,49 +22,173 @@ orchestra <- read_csv(here("data/output.csv")) |>
     across(instrument, ~ gsub(c("percussionist"), "percussion", .)),
     across(instrument, ~ gsub(c("pianist"), "piano", .)),
     across(teachers, ~ gsub("\\; ", ";", .)),
+    across(schools, ~ gsub("\\; ", ";", .)),
+    across(competitions, ~ gsub("\\; ", ";", .)),
+    across(festivals, ~ gsub("\\; ", ";", .)),
+    across(chamber_groups, ~ gsub("\\; ", ";", .)),
+    across(chamber_group_partners, ~ gsub("\\; ", ";", .)),
   ) |> 
   mutate_all(~replace_na(., "unknown"))
 
+# missingness check
 gg_miss_var(orchestra)
 
-#expanded_df <- data.frame(name = character(), 
-#                          instrument = character(),
-#                          teachers = character(), 
-#                          schools = character(),
-#                          competitions = character(),
-#                          festivals = character(),
-#                         chamber_groups = character(),
-#                          chamber_group_partners = character(),
-#                          when_began_current = character(),
-#                          stringsAsFactors = FALSE)
+# teachers
+teachers_df <- data.frame(name = character(),
+                          instrument = character(),
+                          when_began_current = character(),
+                          receiver = character(),
+                          type = character())
 
 for (i in 1:nrow(orchestra)) {
   name_id <- orchestra$name[i]
   instrument_id <- orchestra$instrument[i]
   when_began_current_id <- orchestra$when_began_current[i]
   
-  # Create a data frame with each item as a separate row (unlisted ones have to be combined)
-  # may need help with writing code to label the connection as to teacher, school, chamber groups, or partners
   teachers_list <- unlist(strsplit(orchestra$teachers[i], ";"))
-  schools_list <- unlist(strsplit(orchestra$schools[i], ";"))
-  competitions_list <- unlist(strsplit(orchestra$competitions[i], ";"))
-  festivals_list <- unlist(strsplit(orchestra$festivals[i], ";"))
-  chamber_groups_list <- unlist(strsplit(orchestra$chamber_groups[i], ";"))
-  chamber_group_partners_list <- unlist(strsplit(orchestra$chamber_group_partners[i], ";"))
+  label = "teachers"
   
-  # create df.
-#  temp_df <- data.frame(name = name_id, 
-#                        instrument = instrument_id,
-#                        teachers = teachers_list, 
-#                        schools = schools_list,
-#                        competitions = competitions_list,
-#                        festivals = festivals_list,
-#                        chamber_groups = chamber_groups_list,
-#                        chamber_group_partners = chamber_group_partners_list,
-#                        when_began_current = character(),
-#                        stringsAsFactors = FALSE)
+  temp_df <- data.frame(name = name_id,
+                        instrument = instrument_id,
+                        when_began_current = when_began_current_id,
+                        receiver = teachers_list,
+                        type = label)
   
-  # Add the temp_df to the expanded_df
-  expanded_df <- rbind(expanded_df, temp_df)
+  teachers_df <- rbind(teachers_df, temp_df)
 }
+
+# schools
+schools_df <- data.frame(name = character(),
+                          instrument = character(),
+                          when_began_current = character(),
+                          receiver = character(),
+                          type = character())
+
+for (i in 1:nrow(orchestra)) {
+  name_id <- orchestra$name[i]
+  instrument_id <- orchestra$instrument[i]
+  when_began_current_id <- orchestra$when_began_current[i]
+  
+  schools_list <- unlist(strsplit(orchestra$schools[i], ";"))
+  label = "schools"
+  
+  temp_df <- data.frame(name = name_id,
+                        instrument = instrument_id,
+                        when_began_current = when_began_current_id,
+                        receiver = schools_list,
+                        type = label)
+  
+  schools_df <- rbind(schools_df, temp_df)
+}
+
+# competitions
+competitions_df <- data.frame(name = character(),
+                         instrument = character(),
+                         when_began_current = character(),
+                         receiver = character(),
+                         type = character())
+
+for (i in 1:nrow(orchestra)) {
+  name_id <- orchestra$name[i]
+  instrument_id <- orchestra$instrument[i]
+  when_began_current_id <- orchestra$when_began_current[i]
+  
+  competitions_list <- unlist(strsplit(orchestra$competitions[i], ";"))
+  label = "competitions"
+  
+  temp_df <- data.frame(name = name_id,
+                        instrument = instrument_id,
+                        when_began_current = when_began_current_id,
+                        receiver = competitions_list,
+                        type = label)
+  
+  competitions_df <- rbind(competitions_df, temp_df)
+}
+
+# festivals
+festivals_df <- data.frame(name = character(),
+                              instrument = character(),
+                              when_began_current = character(),
+                              receiver = character(),
+                              type = character())
+
+for (i in 1:nrow(orchestra)) {
+  name_id <- orchestra$name[i]
+  instrument_id <- orchestra$instrument[i]
+  when_began_current_id <- orchestra$when_began_current[i]
+  
+  festivals_list <- unlist(strsplit(orchestra$festivals[i], ";"))
+  label = "festivals"
+  
+  temp_df <- data.frame(name = name_id,
+                        instrument = instrument_id,
+                        when_began_current = when_began_current_id,
+                        receiver = festivals_list,
+                        type = label)
+  
+  festivals_df <- rbind(festivals_df, temp_df)
+}
+
+# chamber groups
+chamber_groups_df <- data.frame(name = character(),
+                           instrument = character(),
+                           when_began_current = character(),
+                           receiver = character(),
+                           type = character())
+
+for (i in 1:nrow(orchestra)) {
+  name_id <- orchestra$name[i]
+  instrument_id <- orchestra$instrument[i]
+  when_began_current_id <- orchestra$when_began_current[i]
+  
+  chamber_groups_list <- unlist(strsplit(orchestra$chamber_groups[i], ";"))
+  label = "chamber_groups"
+  
+  temp_df <- data.frame(name = name_id,
+                        instrument = instrument_id,
+                        when_began_current = when_began_current_id,
+                        receiver = chamber_groups_list,
+                        type = label)
+  
+  chamber_groups_df <- rbind(chamber_groups_df, temp_df)
+}
+
+# chamber group partners
+chamber_group_partners_df <- data.frame(name = character(),
+                                instrument = character(),
+                                when_began_current = character(),
+                                receiver = character(),
+                                type = character())
+
+for (i in 1:nrow(orchestra)) {
+  name_id <- orchestra$name[i]
+  instrument_id <- orchestra$instrument[i]
+  when_began_current_id <- orchestra$when_began_current[i]
+  
+  chamber_group_partners_list <- unlist(strsplit(orchestra$chamber_group_partners[i], ";"))
+  label = "chamber_group_partners"
+  
+  temp_df <- data.frame(name = name_id,
+                        instrument = instrument_id,
+                        when_began_current = when_began_current_id,
+                        receiver = chamber_group_partners_list,
+                        type = label)
+  
+  chamber_group_partners_df <- rbind(chamber_group_partners_df, temp_df)
+}
+
+# bind rows together and filter out unknowns
+orchestra_network_cleanish <- bind_rows(
+  teachers_df,
+  schools_df,
+  festivals_df,
+  competitions_df,
+  chamber_groups_df,
+  chamber_group_partners_df
+) |> 
+  filter(receiver != "unknown") |> 
+  filter(receiver != "Unknown")
+
+save(orchestra_network_cleanish, file = here("data/orchestra_network_cleanish.rda"))
+
 
